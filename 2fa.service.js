5bfd35378916fc1dd89265cbd9e680f536c2eb69
Marius de Jong <36531760+mariuscdejong@users.noreply.github.com>
2022-08-12 10:48:13 +0200

    Update 2fa.service.js

--- 2fa.service.js
+++ 2fa.service.js
@@ -14,41 +14,41 @@ export class TwoFactorAuthService {
     });
   }
 
-  // enable() {
-  //   return this.$axios.$post(`${this.apiBaseUrl}/user/two-factor-authentication`);
-  // }
+  enable() {
+    return this.$axios.$post(`${this.apiBaseUrl}/user/two-factor-authentication`);
+  }
 
-  // confirm(code) {
-  //   return this.$axios.$post(`${this.apiBaseUrl}/user/confirmed-two-factor-authentication`, {
-  //     code,
-  //   });
-  // }
+  confirm(code) {
+    return this.$axios.$post(`${this.apiBaseUrl}/user/confirmed-two-factor-authentication`, {
+      code,
+    });
+  }
 
-  // confirmWithRecoveryCode(code) {
-  //   return this.$axios.$post(`${this.apiBaseUrl}/two-factor-challenge`, {
-  //     recovery_code: code,
-  //   });
-  // }
+  confirmWithRecoveryCode(code) {
+    return this.$axios.$post(`${this.apiBaseUrl}/two-factor-challenge`, {
+      recovery_code: code,
+    });
+  }
 
-  // challenge(code) {
-  //   return this.$axios.$post(`${this.apiBaseUrl}/two-factor-challenge`, {
-  //     code,
-  //   });
-  // }
+  challenge(code) {
+    return this.$axios.$post(`${this.apiBaseUrl}/two-factor-challenge`, {
+      code,
+    });
+  }
 
-  // qrCodeSvg() {
-  //   return this.$axios.$get(`${this.apiBaseUrl}/user/two-factor-qr-code`);
-  // }
+  qrCodeSvg() {
+    return this.$axios.$get(`${this.apiBaseUrl}/user/two-factor-qr-code`);
+  }
 
-  // recoveryCodes() {
-  //   return this.$axios.$get(`${this.apiBaseUrl}/user/two-factor-recovery-codes`);
-  // }
+  recoveryCodes() {
+    return this.$axios.$get(`${this.apiBaseUrl}/user/two-factor-recovery-codes`);
+  }
 
-	// regenerateRecoveryCodes() {
-	// 	return this.$axios.$post(`${this.apiBaseUrl}/user/two-factor-recovery-codes`);
-	// }
+	regenerateRecoveryCodes() {
+		return this.$axios.$post(`${this.apiBaseUrl}/user/two-factor-recovery-codes`);
+	}
 
-  // disable() {
-  //   return this.$axios.$delete(`${this.apiBaseUrl}/user/two-factor-authentication`);
-  // }
+  disable() {
+    return this.$axios.$delete(`${this.apiBaseUrl}/user/two-factor-authentication`);
+  }
 }
